903d3f04837f991e8c56bdae624c5e40f7f6131a
Paul A. Trzyna <ptrzyna@gmail.com>
2015-02-01 20:13:27 -0600

    added code to allow for unit testing of DAOs and services

---
+++ src/test/java/edu/depaul/armada/dao/ContainerDaoTest.java
@@ -0,0 +1,53 @@
+/**
+ * 
+ */
+package edu.depaul.armada.dao;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * @author ptrzyna
+ *
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"/beans/armada-config-test.xml"})
+public class ContainerDaoTest {
+
+	/**
+	 * Test method for {@link edu.depaul.armada.dao.ContainerDao#store(java.lang.Object)}.
+	 */
+	@Test
+	public void testStore() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link edu.depaul.armada.dao.ContainerDao#getAll()}.
+	 */
+	@Test
+	public void testGetAll() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link edu.depaul.armada.dao.ContainerDao#get(long, int)}.
+	 */
+	@Test
+	public void testGet() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link edu.depaul.armada.dao.ContainerDao#findWithDockerId(java.lang.String)}.
+	 */
+	@Test
+	public void testFindWithDockerId() {
+		fail("Not yet implemented");
+	}
+
+}
