8bd14677e7154b93373988fef47a97b057bea865
Roland T. Craddolph <rcraddolph@gmail.com>
2015-02-16 19:28:22 -0600

    Fixed UI and changed UI Test name

---
+++ src/test/java/edu/depaul/armada/ArmadaTest_UI.java
@@ -0,0 +1,45 @@
+package edu.depaul.armada;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+
+/**
+ * @author tokenofid
+ * 
+ */
+@Ignore
+public class ArmadaTest_UI {
+	private WebDriver driver = new FirefoxDriver();
+	final static String URL = "http://localhost:8083";
+	
+	By containerTable = By.id("containers");
+	
+	@Before
+	public void setUp() throws Exception {
+		driver.get(URL);
+	}
+
+	@After
+	public void tearDown(){
+	    driver.quit();
+	}
+	
+	@Test
+	public void initialLoading() throws InterruptedException {
+		assertEquals("Armada Dashboard", driver.getTitle());
+	}
+	
+	@Test
+	public void loadContainers() throws InterruptedException {
+		boolean hasContainers =  driver.findElement(containerTable).isDisplayed();
+		assertTrue(hasContainers);
+	}
+}
