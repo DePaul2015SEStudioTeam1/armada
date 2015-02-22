be7707036e2087ba8051118a80b5bb87d3b02150
Paul A. Trzyna <ptrzyna@gmail.com>
2015-02-21 20:00:58 -0600

    added a POST api for preferences

--- src/main/java/edu/depaul/armada/controller/PreferenceRestfulController.java
+++ src/main/java/edu/depaul/armada/controller/PreferenceRestfulController.java
@@ -2,19 +2,24 @@ package edu.depaul.armada.controller;
 
 import java.util.List;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import edu.depaul.armada.dao.PreferenceDao;
+import edu.depaul.armada.domain.Preference;
 import edu.depaul.armada.model.DashboardPreference;
 
 
 @Controller
 @RequestMapping("/preferences")
 public class PreferenceRestfulController {
+	
+	private final Logger log = Logger.getLogger(getClass());
 	
 	private PreferenceDao preferenceDao;
 	
@@ -29,5 +34,23 @@ public class PreferenceRestfulController {
 		return preferenceDao.getAll();
 	}
 	
+	@RequestMapping(value = "/setAll", method = RequestMethod.POST)
+	public void setAll(@RequestBody List<DashboardPreference> preferences) {
+		for(DashboardPreference temp : preferences) {
+			Preference pref = preferenceDao.findWithPreferenceKey(temp.key);
+			if(pref == null) {
+				pref = new Preference();
+				pref.setKey(temp.key);
+			}
+			pref.setValue(temp.value);
+			try {
+				preferenceDao.storePreference(pref);
+			}
+			catch(Exception e) {
+				log.error("Skipping adding a preference with key: " + temp.key + " due to an error!", e);
+				continue;
+			}
+		}
+	}
 	
 }
