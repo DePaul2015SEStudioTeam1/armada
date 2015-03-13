75402aaa09d295567639e68d38f4b58804603194
Paul A. Trzyna <ptrzyna@gmail.com>
2015-03-12 21:44:50 -0500

    fixed deatil charts

--- src/main/java/edu/depaul/armada/dao/ContainerLogDaoHibernate.java
+++ src/main/java/edu/depaul/armada/dao/ContainerLogDaoHibernate.java
@@ -158,6 +158,10 @@ public class ContainerLogDaoHibernate implements ContainerLogDao {
 	public List<Metric> findWithContainerIdAndPeriod(long containerId, int periodCount, String fieldToAverage) {
 		List<Metric> metrics = new ArrayList<Metric>(periodCount);
 		Calendar cal = Calendar.getInstance();
+		cal.set(Calendar.MINUTE, 0);
+		cal.set(Calendar.SECOND, 0);
+		cal.set(Calendar.MILLISECOND, 0);
+		cal.add(Calendar.HOUR_OF_DAY, 1);
 		for(int i=0; i<periodCount; i++) {
 			Date end = cal.getTime();
 			int hour = cal.get(Calendar.HOUR_OF_DAY);
